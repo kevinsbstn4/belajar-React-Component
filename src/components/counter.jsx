e5b26a55859f9eda223ae002899fa1e93775c969
kevinsbstn4 <kevinsbstn4@gmail.com>
2019-04-01 22:03:43 +0700

    handle changes

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,24 +2,50 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
   state = {
-    count: 0,
-    tags: ['tag1', 'tag2', 'tag3']
+    count: 0
+    // tags: []
   };
+  handleIncrement() {
+    console.log('Increment Clicked', this);
+  }
 
-  renderTags() {
-    if (this.state.tags.length === 0) return <p>There are no tags !</p>;
+  // renderTags() {
+  //   if (this.state.tags.length === 0) return <p>There are no tags !</p>;
 
+  //   return (
+  //     <ul>
+  //       {this.state.tags.map(tag => (
+  //         <li key={tag}>{tag}</li>
+  //       ))}{' '}
+  //     </ul>
+  //   );
+  // }
+
+  render() {
     return (
-      <ul>
-        {this.state.tags.map(tag => (
-          <li key={tag}>{tag}</li>
-        ))}{' '}
-      </ul>
+      <div>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm">
+          Increment
+        </button>
+      </div>
+      // <div>
+      //   {this.state.tags.length === 0 && 'Please create a new tag !'}
+      //   {this.renderTags()}
+      // </div>
     );
   }
 
-  render() {
-    return <div>{this.renderTags()}</div>;
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? 'Zero' : this.state.count;
+  }
+  getBadgeClasses() {
+    let classes = 'badge m-2 badge-';
+    classes += this.state.count === 0 ? 'warning' : 'primary';
+    return classes;
   }
 }
 
@@ -30,19 +56,3 @@ export default Counter;
 //   fontSize: 25
 // };
 // style={this.styles}
-
-{
-  /* <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button> */
-}
-
-// getBadgeClasses() {
-//   let classes = 'badge m-2 badge-';
-//   classes += this.state.count === 0 ? 'warning' : 'primary';
-//   return classes;
-// }
-
-// formatCount() {
-//   const { count } = this.state;
-//   return count === 0 ? 'Zero' : this.state.count;
-// }
