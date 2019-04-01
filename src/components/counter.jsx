e4e6d40d4a6da495fcf3c0f54d5b114ae8162994
kevinsbstn4 <kevinsbstn4@gmail.com>
2019-04-01 21:30:04 +0700

    tag map

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,26 +2,30 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
   state = {
-    count: 1
+    count: 0,
+    tags: ['tag1', 'tag2', 'tag3']
   };
 
-  styles = {
-    fontWeight: 'bold',
-    fontSize: 25
-  };
+  // styles = {
+  //   fontWeight: 'bold',
+  //   fontSize: 25
+  // };
+  // style={this.styles}
   render() {
-    let classes = this.getBadgeClasses();
     return (
       <React.Fragment>
-        <span style={this.styles} className={classes}>
-          {this.formatCount()}
-        </span>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
+        <ul>
+          {this.state.tags.map(tag => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
       </React.Fragment>
     );
   }
 
-  newMethod() {
+  getBadgeClasses() {
     let classes = 'badge m-2 badge-';
     classes += this.state.count === 0 ? 'warning' : 'primary';
     return classes;
